a61681f78996c6b16d23d4a964dadc31542a6a20
Harpreet Singh <singhsharpreet3@gmail.com>
2024-08-15 21:57:17 +0530

    Initial Commit

---
+++ CleanArchitectureExample/CleanArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureExampleApp.swift
+//  CleanArchitectureExample
+//
+//  Created by Harpreet Singh on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
